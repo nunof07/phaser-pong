3f8b2da860ef79a35b0a68d6b1d73cac5a49e060
Nuno Freitas <nunofreitas@gmail.com>
2018-01-28 19:14:57 +0000

    Remove mocha typescript

--- src/test/RandomizerTest.ts
+++ src/test/RandomizerTest.ts
@@ -1,18 +1,16 @@
 import { Randomizer } from '@main/Randomizer';
 import { expect } from 'chai';
-import { suite, test } from 'mocha-typescript';
 
 /**
  * {@link Randomizer} test cases.
  */
-@suite
-export class RandomizerTest {
-    @test
-    public returnsBool(): void {
-        expect(
-            new Randomizer().bool()
-        ).to.satisfy((result: boolean): boolean => {
-            return result === true || result === false;
+describe('Randomizer', () => {
+    describe('#bool()', () => {
+        it('should return a boolean', () => {
+            expect(new Randomizer().bool())
+                .to.satisfy((result: boolean): boolean =>
+                    result === true || result === false
+                );
         });
-    }
-}
+    });
+});
